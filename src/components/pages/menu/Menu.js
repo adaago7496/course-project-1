10eed9045239a9053c5bebdaf41dda037c10eb91
Adam Agostini <adaago7496@students.ecpi.edu>
2022-11-24 20:58:32 -0500

    Order 2

--- src/components/pages/menu/Menu.js
+++ src/components/pages/menu/Menu.js
@@ -11,7 +11,7 @@ import "./Menu.css";
 //const newOrderDetails=JSON.stringify([...orderDetails,{name:"new item"}]);
 //localStorage.setItem("myItems",newOrderDetails);
 
-const orderDetails=[
+var orderDetails=[
     {
         id:0,
         pizzaType: "Cheese",
@@ -24,11 +24,15 @@ const orderDetails=[
     }];
 
     function addToOrder(pizzaId){
+        var quantity=Number(localStorage.getItem("Cheese"));
+        console.log(quantity);
+        localStorage.setItem("Cheese",quantity++);
         //var tempItem=localStorage.getItem(pizzaId);
         //localStorage.setItem(tempItem.pizzaType,tempItem.quantity++);
-        orderDetails[pizzaId].quantity++;
+        //orderDetails[pizzaId].quantity++;
         //console.log(orderDetails[pizzaId].quantity);
-        localStorage.setItem(orderDetails[pizzaId].pizzaType,orderDetails[pizzaId].quantity);
+        //localStorage.setItem(orderDetails[pizzaId].pizzaType,orderDetails[pizzaId].quantity);
+       // localStorage.setItem(orderDetails[pizzaId].pizzaType,orderDetails[pizzaId].quantity++);
     }
 
 class Menu extends Component{
@@ -36,8 +40,9 @@ class Menu extends Component{
         return(
             <div>
                 <h2>Menu</h2>
-                <ul className="content">                
-                        <li><Link to={{pathname: "../components/pages/cart/Checkout"}}><img alt="cheese" src={cheese} onClick={addToOrder("Cheese")}/>Cheese Pizza: {localStorage.getItem("Cheese")}</Link></li>
+                <ul className="content">   
+                        <li>{orderDetails[0].pizzaType}</li>
+                        <li><Link to={{pathname: "../components/pages/cart/Checkout"}}><img alt="cheese" src={cheese} onClick={addToOrder(0)}/>Cheese Pizza: {localStorage.getItem("Cheese")}</Link></li>
                         <li><NavLink to="../components/pages/cart/Checkout"><img alt="pepperoni" src={pepperoni} onClick={addToOrder("Pepperoni")}/>Pepperoni Pizza</NavLink></li>
                 </ul>
             </div>
