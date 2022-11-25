a540d60373975d35c6920651161e2fdd2a3523b6
Adam Agostini <adaago7496@students.ecpi.edu>
2022-11-24 21:25:21 -0500

    Trying to determine how to remove strict mode and not have functions run when the page loads.

--- src/components/pages/menu/Menu.js
+++ src/components/pages/menu/Menu.js
@@ -23,10 +23,33 @@ var orderDetails=[
         quantity:0
     }];
 
-    function addToOrder(pizzaId){
+    function createOrder(){
+        localStorage.setItem("Cheese",0);
+        localStorage.setItem("Pepperoni",0);
+    }
+
+
+    function increaseCheese(){
         var quantity=Number(localStorage.getItem("Cheese"));
-        console.log(quantity);
-        localStorage.setItem("Cheese",quantity++);
+        quantity++;
+        localStorage.setItem("Cheese",quantity.toString());
+        console.log("Pizza: Cheese"+" quantity: "+quantity);
+        //localStorage.setItem("Cheese",quantity+1);
+        //var tempItem=localStorage.getItem(pizzaId);
+        //localStorage.setItem(tempItem.pizzaType,tempItem.quantity++);
+        //orderDetails[pizzaId].quantity++;
+        //console.log(orderDetails[pizzaId].quantity);
+        //localStorage.setItem(orderDetails[pizzaId].pizzaType,orderDetails[pizzaId].quantity);
+       // localStorage.setItem(orderDetails[pizzaId].pizzaType,orderDetails[pizzaId].quantity++);
+    }
+
+
+    function increasePepperoni(){
+        var quantity=Number(localStorage.getItem("Pepperoni"));
+        quantity++;
+        localStorage.setItem("Pepperoni",quantity.toString());
+        console.log("Pizza: Pepperoni"+" quantity: "+quantity);
+        //localStorage.setItem("Cheese",quantity+1);
         //var tempItem=localStorage.getItem(pizzaId);
         //localStorage.setItem(tempItem.pizzaType,tempItem.quantity++);
         //orderDetails[pizzaId].quantity++;
@@ -36,14 +59,27 @@ var orderDetails=[
     }
 
 class Menu extends Component{
+    increaseCheese(){
+        var quantity=Number(localStorage.getItem("Cheese"));
+        quantity++;
+        localStorage.setItem("Cheese",quantity.toString());
+        console.log("Pizza: Cheese"+" quantity: "+quantity);
+        //localStorage.setItem("Cheese",quantity+1);
+        //var tempItem=localStorage.getItem(pizzaId);
+        //localStorage.setItem(tempItem.pizzaType,tempItem.quantity++);
+        //orderDetails[pizzaId].quantity++;
+        //console.log(orderDetails[pizzaId].quantity);
+        //localStorage.setItem(orderDetails[pizzaId].pizzaType,orderDetails[pizzaId].quantity);
+       // localStorage.setItem(orderDetails[pizzaId].pizzaType,orderDetails[pizzaId].quantity++);
+    }
+
     render(){         
         return(
             <div>
                 <h2>Menu</h2>
                 <ul className="content">   
-                        <li>{orderDetails[0].pizzaType}</li>
-                        <li><Link to={{pathname: "../components/pages/cart/Checkout"}}><img alt="cheese" src={cheese} onClick={addToOrder(0)}/>Cheese Pizza: {localStorage.getItem("Cheese")}</Link></li>
-                        <li><NavLink to="../components/pages/cart/Checkout"><img alt="pepperoni" src={pepperoni} onClick={addToOrder("Pepperoni")}/>Pepperoni Pizza</NavLink></li>
+                        <li><Link to={{pathname: "../components/pages/cart/Checkout"}}><img alt="cheese" src={cheese} onClick={increaseCheese()}/>Cheese Pizza: {localStorage.getItem("Cheese")}</Link></li>
+                        <li><NavLink to="../components/pages/cart/Checkout"><img alt="pepperoni" src={pepperoni} onClick={increasePepperoni()}/>Pepperoni Pizza: {localStorage.getItem("Pepperoni")}</NavLink></li>
                 </ul>
             </div>
         );
